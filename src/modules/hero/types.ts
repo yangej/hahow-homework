c0952615284b869750bf2148800d2fb2b0db902b
ejyang <yangej0218@gmail.com>
2024-07-03 14:48:57 +0800

    feat(modules/hero): add basic api interfaces

---
+++ src/modules/hero/types.ts
@@ -0,0 +1,12 @@
+export type Hero = {
+    id: string;
+    name: string;
+    image: string;
+}
+
+export type HeroProfile = {
+    str: number;
+    int: number;
+    agi: number;
+    luk: number;
+}

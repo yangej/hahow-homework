156b33dca73ddfcfeb16a16e8b7ded630012864c
ejyang <yangej0218@gmail.com>
2024-07-04 20:00:24 +0800

    feat(modules/hero): add get hero profile query hook

--- src/modules/hero/hooks.ts
+++ src/modules/hero/hooks.ts
@@ -1,6 +1,7 @@
 import { useQuery, queryOptions } from "@tanstack/react-query";
-import { listHero } from "./apis";
+import { getHeroProfile, listHero } from "./apis";
 import { queryKeyFactory } from "@/libs/react-query/utils";
+import { Hero } from "./types";
 
 const heroQueryKeys = queryKeyFactory("hero");
 
@@ -11,4 +12,17 @@ const heroesQueryOptions = queryOptions({
 
 export const useHeroes = () => {
   return useQuery(heroesQueryOptions);
+};
+
+const heroProfileKeys = queryKeyFactory("heroProfile");
+
+const heroProfileQueryOptions = (id: Hero["id"]) =>
+  queryOptions({
+    queryKey: heroProfileKeys.item(id),
+    queryFn: () => getHeroProfile({ id }),
+    staleTime: 1000 * 60 * 5, // 5 mins
+  });
+
+export const useHeroProfile = (id: Hero["id"]) => {
+  return useQuery(heroProfileQueryOptions(id));
 };

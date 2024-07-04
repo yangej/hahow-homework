b07c4d9d2e00daf2282ddb79c04376b662842a0b
ejyang <yangej0218@gmail.com>
2024-07-04 18:49:17 +0800

    feat(modules/hero): add hero apis

---
+++ src/modules/hero/apis.ts
@@ -0,0 +1,29 @@
+import * as clients from "@/modules/common/utils/api";
+import type {
+  GetHeroPayload,
+  GetHeroProfilePayload,
+  GetHeroProfileResponse,
+  GetHeroResponse,
+  ListHeroResponse,
+  PatchHeroProfilePayload,
+  PatchHeroProfileResponse,
+} from "./types";
+
+const listHero = () => clients.hahow.get<ListHeroResponse>("/heroes");
+
+const getHero = ({ id }: GetHeroPayload) => {
+  return clients.hahow.get<GetHeroResponse>(`/heroes/${id}`);
+};
+
+const getHeroProfile = ({ id }: GetHeroProfilePayload) => {
+  return clients.hahow.get<GetHeroProfileResponse>(`/heroes/${id}/profile`);
+};
+
+const patchHeroProfile = ({ id, ...body }: PatchHeroProfilePayload) => {
+  return clients.hahow.patch<PatchHeroProfileResponse, typeof body>(
+    `/heroes/${id}/profile`,
+    body
+  );
+};
+
+export { listHero, getHero, getHeroProfile, patchHeroProfile };
--- src/modules/hero/types.ts
+++ src/modules/hero/types.ts
@@ -1,12 +1,32 @@
 export type Hero = {
-    id: string;
-    name: string;
-    image: string;
-}
+  id: string;
+  name: string;
+  image: string;
+};
+
+export type ListHeroResponse = Hero[];
+
+export type GetHeroPayload = {
+  id: Hero["id"];
+};
+
+export type GetHeroResponse = Hero;
 
 export type HeroProfile = {
-    str: number;
-    int: number;
-    agi: number;
-    luk: number;
-}
+  str: number;
+  int: number;
+  agi: number;
+  luk: number;
+};
+
+export type GetHeroProfilePayload = {
+  id: Hero["id"];
+};
+
+export type GetHeroProfileResponse = HeroProfile;
+
+export type PatchHeroProfilePayload = {
+  id: Hero["id"];
+} & Partial<HeroProfile>;
+
+export type PatchHeroProfileResponse = string;

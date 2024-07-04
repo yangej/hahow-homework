241dd7b766842a2052d6653060e166357821c50d
ejyang <yangej0218@gmail.com>
2024-07-05 00:31:41 +0800

    feat(modules/app): aggregate colors

---
+++ src/modules/app/constants/theme.ts
@@ -0,0 +1,9 @@
+export const colors = {
+    main: 'rgb(0, 190, 164)',
+    mainLight: 'rgb(220, 249, 243)',
+    warning: 'rgb(246, 95, 85)',
+    warningLight: 'rgb(255, 245, 245)',
+    text: 'rgb(0, 0, 0)',
+    secondaryText: 'rgb(89, 89, 89)',
+    background: 'rgb(245, 247, 249)'
+} as const;

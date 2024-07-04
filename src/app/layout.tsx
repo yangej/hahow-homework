d58eb7c2be4120498b5b4cffac8baa66b39450c7
ejyang <yangej0218@gmail.com>
2024-07-04 19:25:12 +0800

    feat(app/rootLayout): apply hero list

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import type { Params } from "./types";
 import Providers from "./Providers";
 import "./global.css";
+
+import HeroList from "@/modules/hero/components/HeroList";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,17 +15,24 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
+  params,
   children,
 }: Readonly<{
+  params: Params;
   children: React.ReactNode;
 }>) {
+  const heroesId = params.heroesId;
+
   return (
     <html lang="en">
       <body
         className={inter.className}
         style={{ backgroundColor: "rgb(245, 247, 249)" }}
       >
-        <Providers>{children}</Providers>
+        <Providers>
+          <HeroList currentId={heroesId} />
+          {children}
+        </Providers>
       </body>
     </html>
   );
